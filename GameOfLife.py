0c2d5c109ece00716af1cef1e11533ac92a48019
Thomas D <tdietert@mail.usf.edu>
2014-06-01 13:08:19 -0400

    Added reset button to GameofLife
    did some reformatting and added a reset button so you don't have to exit
    the application to restart

--- GameOfLife.py
+++ GameOfLife.py
@@ -17,6 +17,7 @@ class GameOfLife(Frame):
 		self.size_x = 40
 		self.size_y = 28
 		self.cell_buttons = []
+		self.generate_next = True
 
 		self.initialUI()
 
@@ -26,7 +27,7 @@ class GameOfLife(Frame):
 
 		# frame for title and line of instruction
 		self.title_frame = Frame(self.parent)
-		self.title_frame.grid(row = 0, column = 0, columnspan = 3)
+		self.title_frame.grid(row = 0, column = 0, columnspan = 4)
 
 		self.titleFont = font.Font(family="Helvetica", size=14)
 		title = Label(self.title_frame, text = "Conway's Game of Life", font = self.titleFont)
@@ -40,14 +41,17 @@ class GameOfLife(Frame):
 
 		# creates a button to start the simulation
 		self.start_button = Button(self.parent, text = "Start Game", command = self.simulate_game)
-		self.start_button.grid(row = 1, column = 0)
+		self.start_button.grid(row = 1, column = 1, sticky = E)
+
+		self.reset_button = Button(self.parent, text = "Reset", state = DISABLED, command = self.reset_game)
+		self.reset_button.grid(row =1 , column = 2, sticky = W)
 
 	def build_grid(self):
 
 		# creates new frame for grid of cells in game
 		self.game_frame = Frame(
 			self.parent, width = self.size_x + 2, height = self.size_y + 2, borderwidth = 1, relief = SUNKEN)
-		self.game_frame.grid(row = 2, column = 0)
+		self.game_frame.grid(row = 2, column = 0, columnspan = 4)
 
 		#instantiates buttons for choosing initial configuration
 		self.cell_buttons = [[Button(self.game_frame, bg = "white", width = 2, height = 1) for i in range(self.size_x + 2)] for j in range(self.size_y + 2)]
@@ -60,8 +64,7 @@ class GameOfLife(Frame):
 	def simulate_game(self):
 
 		self.disable_buttons()
-
-		# while user does not hit stop_button
+		# creates list of buttons in grid to toggle
 		buttons_to_toggle = []
 		for i in range(1, self.size_y + 1):
 			for j in range(1, self.size_x + 1):
@@ -73,11 +76,14 @@ class GameOfLife(Frame):
 				elif self.cell_buttons[i][j]['bg'] == "black" and self.neighbor_count(i, j) != 3 and self.neighbor_count(i, j) != 2:
 					buttons_to_toggle.append(coord)
 
-		for coord in buttons_to_toggle:
-			self.cell_toggle(self.cell_buttons[coord[0]][coord[1]])
-
 		# updates (toggles) the cells on the grid
-		self.after(50, self.simulate_game)
+		for coord in buttons_to_toggle:
+			self.cell_toggle(self.cell_buttons[coord[0]][coord[1]])			
+
+		if self.generate_next:
+			self.after(100, self.simulate_game)
+		else:
+			self.enable_buttons()
 
 	def disable_buttons(self):
 
@@ -85,6 +91,20 @@ class GameOfLife(Frame):
 			for i in range(0, self.size_y + 2):
 				for j in range(0, self.size_x + 2):
 					self.cell_buttons[i][j].configure(state = DISABLED)
+
+			self.reset_button.configure(state = NORMAL)
+			self.start_button.configure(state = DISABLED)
+
+	def enable_buttons(self):
+		# resets game
+		for i in range(0, self.size_y + 2):
+			for j in range(0, self.size_x + 2):
+				self.cell_buttons[i][j]['bg'] = "white"
+				self.cell_buttons[i][j].configure(state = NORMAL)
+
+		self.reset_button.configure(state = DISABLED)
+		self.start_button.configure(state = NORMAL)
+		self.generate_next = True
 
 	def neighbor_count(self, x_coord, y_coord):
 		count = 0
@@ -100,6 +120,10 @@ class GameOfLife(Frame):
 			cell['bg'] = "black"
 		else:
 			cell['bg'] = "white"
+
+	def reset_game(self):
+		self.generate_next = False
+
 
 if __name__ == '__main__':
 	root = Tk()
